034ef5fed88a6504b327e0fbb5b31cfda0a1cbc2
kashevsky <kashevsky.d@yandex.ru>
2023-02-01 20:42:00 +0300

    Модели с сидеры для продуктов и категорий

---
+++ database/migrations/2023_02_01_162127_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('preview_image')->nullable();
+            $table->text('content')->nullable();
+            $table->float('price')->nullable();
+            $table->string('engraving_depth')->nullable();
+            $table->string('material')->nullable();
+            $table->string('width')->nullable();
+            $table->string('accessories')->nullable();
+            $table->string('form')->nullable();
+            $table->string('garanty')->nullable();
+            $table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
