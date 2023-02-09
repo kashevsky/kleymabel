0bff2543c983a62dbb3840bbde5a05a5c0b9ef14
kashevsky <kashevsky.d@yandex.ru>
2023-02-09 14:17:46 +0300

    MIddleware для входа в админ панель

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
 use Database\Seeders\ProductSeeder;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\SubProductSeeder;
@@ -19,5 +20,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([CategorySeeder::class]);
         $this->call([ProductSeeder::class]);
+        $this->call([UserSeeder::class]);
     }
 }
